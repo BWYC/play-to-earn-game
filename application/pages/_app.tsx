a122a16f5c1e7f472859c42f9f9ca32304a1bf73
BWYC <129849126+BWYC@users.noreply.github.com>
2023-09-27 15:14:00 +0000

    Update 9 files

--- application/pages/_app.tsx
+++ application/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { CoreBlockchain } from "@thirdweb-dev/chains";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const activeChain = "mumbai";
+  const activeChain = CoreBlockchain;
 
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider
+      clientId="65a85b91315ca838d7a8472fb0e64f92"
+      activeChain={activeChain}
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
